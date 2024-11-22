3a3f214af0827790d902090dba2d4afc795a7a0d
nikitalita <69168929+nikitalita@users.noreply.github.com>
2024-11-22 14:06:19 -0600

    add wget_sync

--- utility/common.h
+++ utility/common.h
@@ -14,6 +14,7 @@ void get_strings_from_variant(const Variant &p_var, Vector<String> &r_strings, c
 Error decompress_image(const Ref<Image> &img);
 String get_md5_for_dir(const String &dir, bool ignore_code_signature = false);
 Error unzip_file_to_dir(const String &zip_path, const String &output_dir);
+Error wget_sync(const String &p_url, Vector<uint8_t> &response);
 Error download_file_sync(const String &url, const String &output_path);
 
 template <class T>
