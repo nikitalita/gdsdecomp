0d726dfa825d35b0ae32e30abfdaf4e31e3efcbe
nikitalita <69168929+nikitalita@users.noreply.github.com>
2024-12-08 00:22:41 -0800

    count errors in logger

--- utility/gdre_logger.h
+++ utility/gdre_logger.h
@@ -1,11 +1,14 @@
 #pragma once
 
 #include "core/io/logger.h"
+#include "core/templates/safe_refcount.h"
 
 class GDRELogger : public Logger {
 	Ref<FileAccess> file;
 	String base_path;
 	bool disabled = false;
+	static std::atomic<uint64_t> error_count;
+	thread_local static uint64_t thread_error_count;
 
 public:
 	String get_path() { return base_path; };
@@ -14,6 +17,7 @@ public:
 	void close_file();
 	void _disable(); // only used for during cleanup, because we can't remove the logger
 	virtual void logv(const char *p_format, va_list p_list, bool p_err) _PRINTF_FORMAT_ATTRIBUTE_2_0;
-
+	static uint64_t get_error_count();
+	static uint64_t get_thread_error_count();
 	virtual ~GDRELogger();
 };
