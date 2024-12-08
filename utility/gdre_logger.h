b58c319e3643d2cf5dd399461579febbfd54b57d
nikitalita <69168929+nikitalita@users.noreply.github.com>
2024-12-08 01:22:19 -0800

    improve logging by pre-buffering before log open and sanitizing PII paths

--- utility/gdre_logger.h
+++ utility/gdre_logger.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "core/io/logger.h"
-#include "core/templates/safe_refcount.h"
 
 class GDRELogger : public Logger {
 	Ref<FileAccess> file;
@@ -9,10 +8,16 @@ class GDRELogger : public Logger {
 	bool disabled = false;
 	static std::atomic<uint64_t> error_count;
 	thread_local static uint64_t thread_error_count;
+	bool is_prebuffering = false;
+	Mutex buffer_mutex;
+	Vector<String> buffer;
 
 public:
 	String get_path() { return base_path; };
 	GDRELogger();
+	bool is_prebuffering_enabled() { return is_prebuffering; }
+	void start_prebuffering();
+	void stop_prebuffering();
 	Error open_file(const String &p_base_path);
 	void close_file();
 	void _disable(); // only used for during cleanup, because we can't remove the logger
