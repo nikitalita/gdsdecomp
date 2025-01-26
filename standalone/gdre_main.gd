6caa63932198fac34a8d988cd9c3a2c067c96ecb
nikitalita <69168929+nikitalita@users.noreply.github.com>
2025-01-26 10:49:02 -0800

    fix starting from another scene in the debugger

--- standalone/gdre_main.gd
+++ standalone/gdre_main.gd
@@ -891,7 +891,7 @@ func get_sanitized_args():
 	var args = OS.get_cmdline_args()
 	#var scene_path = get_tree().root.scene_file_path
 	var scene_path = "res://gdre_main.tscn"
-	if args.size() > 0 and args[0] == scene_path:
+	if args.size() > 0 and args[0].begins_with("res://gdre") and args[0].ends_with(".tscn"):
 		return args.slice(1)
 	return args
 
