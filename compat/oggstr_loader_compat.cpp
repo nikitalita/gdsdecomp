1b45f248fe04faa91bc82117a68f6e79f433cf4e
nikitalita <69168929+nikitalita@users.noreply.github.com>
2025-02-13 12:06:26 -0800

    fix linking error on linux

--- compat/oggstr_loader_compat.cpp
+++ compat/oggstr_loader_compat.cpp
@@ -1,13 +1,13 @@
 #include "oggstr_loader_compat.h"
 #include "core/error/error_macros.h"
-#include "modules/vorbis/resource_importer_ogg_vorbis.h"
+#include "modules/vorbis/audio_stream_ogg_vorbis.h"
 
 Ref<Resource> OggStreamConverterCompat::convert(const Ref<MissingResource> &res, ResourceInfo::LoadType p_type, int ver_major, Error *r_error) {
 	String name = get_resource_name(res, ver_major);
 	Vector<uint8_t> data = res->get("data");
 	bool loop = res->get("loop");
 	double loop_offset = res->get("loop_offset");
-	Ref<AudioStreamOggVorbis> sample = ResourceImporterOggVorbis::load_from_buffer(data);
+	Ref<AudioStreamOggVorbis> sample = AudioStreamOggVorbis::load_from_buffer(data);
 	ERR_FAIL_COND_V_MSG(sample.is_null(), res, "Failed to load Ogg Vorbis stream from buffer.");
 	if (!name.is_empty()) {
 		sample->set_name(name);
