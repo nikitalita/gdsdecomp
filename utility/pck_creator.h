225703cc262ab8f83c5ec7e9c29fd1ee9904b508
nikitalita <69168929+nikitalita@users.noreply.github.com>
2025-04-21 14:46:03 -0400

    make pck_creator use task manager

--- utility/pck_creator.h
+++ utility/pck_creator.h
@@ -3,11 +3,6 @@
 
 #include "core/object/object.h"
 #include "core/object/ref_counted.h"
-#include "core/object/worker_thread_pool.h"
-
-#include "utility/gdre_progress.h"
-
-#include "gd_parallel_hashmap.h"
 #include "packed_file_info.h"
 
 #include <core/variant/typed_dictionary.h>
@@ -40,44 +35,40 @@ class PckCreator : public RefCounted {
 	uint64_t offset;
 	std::atomic<bool> cancelled = false;
 	String error_string;
-	std::atomic<int64_t> last_completed = 0;
-	std::atomic<int64_t> skipped_cnt = 0;
 	std::atomic<int64_t> broken_cnt = 0;
 	std::atomic<int64_t> data_read = 0;
 	Vector<String> tmp_files;
+	Ref<FileAccess> f;
+	size_t file_base = 0;
+	Error encryption_error = OK;
+	Vector<uint8_t> key;
 	static constexpr size_t piecemeal_read_size = 65536; //1 * 1024 * 1024;
 	static constexpr size_t _file_is_large = 100 * 1024 * 1024;
 	static constexpr bool is_file_large(size_t size) { return size > _file_is_large; }
 
 	bool _pck_file_check_md5(Ref<PackedFileInfo> &file);
-	void _do_md5_check(uint32_t i, Ref<PackedFileInfo> *tokens);
 	void reset();
-	struct ProcessFolderToken {
-		String path;
-		size_t file_size = 0;
-		uint8_t md5[16] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-		Error err = OK;
-	};
 	void _do_process_folder(uint32_t i, File *tokens);
+	String get_file_description(int64_t i, File *userdata);
+
+	void _do_write_file(uint32_t i, File *tokens);
 
 	inline Error read_and_write_file(size_t i, Ref<FileAccess> write_handle);
-	Error wait_for_task(WorkerThreadPool::GroupID group_task, const Vector<String> &paths_to_check, EditorProgressGDDC *pr);
 	Error headless_pck_create(const String &pck_path, const String &dir, const Vector<String> &include_filters, const Vector<String> &exclude_filters);
 	Error non_headless_pck_create(const String &pck_path, const String &dir, const Vector<String> &include_filters, const Vector<String> &exclude_filters);
 
 protected:
 	static void _bind_methods();
-	String get_file_description(int64_t i, File *userdata);
 
 public:
 	static Vector<String> get_files_to_pack(const String &p_dir, const Vector<String> &include_filters, const Vector<String> &exclude_filters);
-	Error _process_folder(const String &p_pck_path, const String &p_dir, const Vector<String> &file_paths_to_pack, EditorProgressGDDC *pr, String &error_string);
+	Error _process_folder(const String &p_pck_path, const String &p_dir, const Vector<String> &file_paths_to_pack);
 	void start_pck(const String &p_pck_path, int pck_version, int ver_major, int ver_minor, int ver_rev, bool encrypt = false, bool embed = false, const String &exe_to_embed = "", const String &watermark = "");
 	Error add_files(Dictionary file_paths_to_pack);
-	Error _add_files(const HashMap<String, String> &file_paths_to_pack, EditorProgressGDDC *pr);
+	Error _add_files(const HashMap<String, String> &file_paths_to_pack);
 	Error finish_pck();
 	Error pck_create(const String &p_pck_path, const String &p_dir, const Vector<String> &include_filters, const Vector<String> &exclude_filters);
-	Error _create_after_process(EditorProgressGDDC *pr, String &error_string);
+	Error _create_after_process();
 	void set_multi_thread(bool multi_thread) { opt_multi_thread = multi_thread; }
 	bool get_multi_thread() const { return opt_multi_thread; }
 	void set_pack_version(int ver) { version = ver; }
