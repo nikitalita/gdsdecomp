5afdce7c9724b9c92b2b223ca1bfa5b1bb1249b8
nikitalita <69168929+nikitalita@users.noreply.github.com>
2025-07-18 16:41:29 -0700

    seperate dotnetcoredepinfo out into seperate file

--- external/godot-mono-decomp/GodotMonoDecomp/ProjectFileWriterGodotStyle.cs
+++ external/godot-mono-decomp/GodotMonoDecomp/ProjectFileWriterGodotStyle.cs
@@ -111,169 +111,6 @@ namespace GodotMonoDecomp
 			xml.WriteEndElement();
 		}
 
-		class DotNetCoreDepInfo
-		{
-			public readonly string Name;
-			public readonly string Version;
-			public readonly string Type;
-			public readonly string Path;
-			public readonly string Sha512;
-			public readonly bool Serviceable;
-			public readonly DotNetCoreDepInfo[] deps;
-			public readonly string[] runtimeComponents;
-
-			public DotNetCoreDepInfo(string fullName, string version, string type, bool serviceable, string path,
-				string sha512,
-				DotNetCoreDepInfo[] deps, string[] runtimeComponents)
-			{
-				var parts = fullName.Split('/');
-				this.Name = parts[0];
-				if (parts.Length > 1)
-				{
-					this.Version = parts[1];
-				}
-				else
-				{
-					this.Version = version;
-				}
-
-				this.Type = type;
-				this.Serviceable = serviceable;
-				this.Path = path;
-				this.Sha512 = sha512;
-
-				this.deps = deps;
-				this.runtimeComponents = runtimeComponents;
-			}
-
-			static public DotNetCoreDepInfo Create(string fullName, string version, string target, JsonObject blob)
-			{
-				return Create(fullName, version, target, blob, new HashSet<string>());
-			}
-
-			static DotNetCoreDepInfo Create(string fullName, string version, string target, JsonObject blob,
-				HashSet<string> _deps)
-			{
-				var parts = fullName.Split('/');
-				var Name = parts[0];
-				var Version = "<UNKNOWN>";
-				if (parts.Length > 1)
-				{
-					Version = parts[1];
-				}
-				else
-				{
-					Version = version;
-				}
-
-				var type = "runtimedll";
-				var serviceable = false;
-				var path = "";
-				var sha512 = "";
-				var libraryBlob = blob["libraries"][Name + "/" + Version].AsJsonObject;
-				if (libraryBlob != null)
-				{
-					type = libraryBlob["type"].AsString;
-					serviceable = libraryBlob["serviceable"].AsBoolean;
-					path = libraryBlob["path"].AsString ?? "";
-					sha512 = libraryBlob["sha512"].AsString ?? "";
-				}
-
-				string[] runtimeComponents = Array.Empty<string>();
-				var runtimeBlob = blob["targets"][target].AsJsonObject?[Name + "/" + Version].AsJsonObject?["runtime"]
-					.AsJsonObject;
-				if (runtimeBlob != null)
-				{
-					runtimeComponents = new string[runtimeBlob.Count];
-					int i = 0;
-					foreach (var component in runtimeBlob)
-					{
-						runtimeComponents[i] = System.IO.Path.GetFileNameWithoutExtension(component.Key);
-						i++;
-					}
-				}
-
-				var deps = getDeps(Name, Version, target, blob, _deps);
-				return new DotNetCoreDepInfo(Name, Version, type, serviceable, path, sha512, deps, runtimeComponents);
-			}
-
-
-			static DotNetCoreDepInfo[] getDeps(string Name, string Version, string target, JsonObject blob,
-				HashSet<string>? _deps = null)
-			{
-				if (_deps == null)
-				{
-					_deps = new HashSet<string>();
-				}
-
-				var targetBlob = blob["targets"][target].AsJsonObject;
-				if (targetBlob == null)
-				{
-					return Empty<DotNetCoreDepInfo>.Array;
-				}
-
-				var depsBlob = targetBlob[Name + "/" + Version].AsJsonObject?["dependencies"].AsJsonObject;
-				var runtimeBlob = targetBlob[Name + "/" + Version].AsJsonObject?["runtime"].AsJsonObject;
-				if (depsBlob == null && runtimeBlob == null)
-				{
-					return Empty<DotNetCoreDepInfo>.Array;
-				}
-
-				List<DotNetCoreDepInfo> result = new List<DotNetCoreDepInfo>();
-				Dictionary<String, String> deps = new Dictionary<string, string>();
-				if (depsBlob != null)
-				{
-					foreach (var dep in depsBlob)
-					{
-						if (!_deps.Add(dep.Key + "/" + dep.Value.AsString))
-						{
-							continue;
-						}
-
-						deps.Add(dep.Key, dep.Value.AsString);
-					}
-				}
-
-				for (int i = 0; i < deps.Count; i++)
-				{
-					var dep = deps.ElementAt(i);
-					var new_dep = Create(dep.Key, dep.Value, target, blob, _deps);
-					result.Add(new_dep);
-				}
-
-				return result.ToArray();
-			}
-
-			public bool HasDep(string name, bool parentIsPackage)
-			{
-				if (this.runtimeComponents.Contains(name))
-				{
-					return true;
-				}
-				for (int i = 0; i < deps.Length; i++)
-				{
-					if (parentIsPackage && (deps[i].Type != "package"))
-					{
-						continue;
-					}
-
-					if (deps[i].Name == name)
-					{
-						return true;
-					}
-
-					if (deps[i].HasDep(name, false))
-					{
-						return true;
-					}
-				}
-
-				return false;
-			}
-
-			public string PathName { get => System.IO.Path.Combine(Name, Version); }
-		}
-
 		class DotNetCorePackageInfo
 		{
 			public readonly string Name;
@@ -332,30 +169,21 @@ namespace GodotMonoDecomp
 		{
 			// remove the .dll extension
 			var depsJsonFileName = module.FileName.Substring(0, module.FileName.Length - 4) + ".deps.json";
+			DotNetCoreDepInfo? depInfo = null;
 			try
 			{
-				var dependencies = JsonReader.Parse(File.ReadAllText(depsJsonFileName));
-				// go through each target framework, find the one that matches the module
-				Dictionary<String, JsonValue> targetBlob = new Dictionary<string, JsonValue>();
-				foreach (var target in dependencies["targets"].AsJsonObject)
-				{
-					foreach (var dependency in target.Value.AsJsonObject)
-					{
-						if (dependency.Key.StartsWith(module.Name))
-						{
-							return DotNetCoreDepInfo.Create(dependency.Key, "", target.Key, dependencies.AsJsonObject);
-						}
-					}
-				}
+				depInfo = DotNetCoreDepInfo.LoadDepInfoFromFile(depsJsonFileName, module.Name);
 			}
 			catch (Exception e)
 			{
 				Console.Error.WriteLine($"Error reading {depsJsonFileName}: {e.Message}");
 			}
+			if (depInfo == null)
+			{
+				Console.Error.WriteLine($"Could not load dependency information for module {module.FileName}");
+			}
 
-			Console.Error.WriteLine($"Could not load dependency information for module {module.FileName}");
-
-			return null;
+			return depInfo;
 		}
 
 		static void WritePackageReferences(XmlTextWriter xml, MetadataFile module, IProjectInfoProvider project,
