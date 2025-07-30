9035903c5319be34e403740b24c788e6d0197d3b
nikitalita <69168929+nikitalita@users.noreply.github.com>
2025-07-30 16:03:16 -0700

    soup up CLI

--- external/godot-mono-decomp/GodotMonoDecompCLI/Program.cs
+++ external/godot-mono-decomp/GodotMonoDecompCLI/Program.cs
@@ -2,29 +2,91 @@
 
 using GodotMonoDecomp;
 
+// import a CLI parser
+using CommandLine;
+
 // specify this is the main entry point
 int Main(string[] args)
 {
+	var result = CommandLine.Parser.Default.ParseArguments<Options>(args);
+	if (result.Errors.Any())
+	{
+		Console.WriteLine("Error: " + result.Errors.First().ToString());
 
-    if (args.Length < 3)
-    {
-        Console.WriteLine("Usage: GodotMonoDecomp-cli <assembly-path> <output-path> <project-path> [<reference-paths>...]");
-        return 1;
-    }
+		return 1;
+	}
+	if (string.IsNullOrWhiteSpace(result.Value.AssemblyPath))
+	{
+		Console.WriteLine("Error: Assembly path is required");
+		return 1;
+	}
+
+
     // get the assembly path from the command line
-    string assemblyPath = args[0];
+    string assemblyPath = Path.GetFullPath(result.Value.AssemblyPath!);
 
     // get the output path from the command line
-    string outputPath = args[1];
+    string outputDir = result.Value.OutputProject ?? Path.GetDirectoryName(assemblyPath)!;
 
-    string projectPath = args[2];
+	string assemblyName = result.Value.ProjectName ?? Path.GetFileNameWithoutExtension(assemblyPath);
+
+	string outputCSProj = Path.Combine(outputDir, assemblyName + ".csproj");
+
+    string projectPath = result.Value.ExtractedProject ?? outputDir;
     // get the reference paths from the command line
-    string[] referencePaths = args.Skip(3).ToArray();
+    string[] referencePaths = result.Value.ReferencePath != null ? [Path.GetFullPath(result.Value.ReferencePath)] : [];
+
+	GodotMonoDecompSettings settings = new GodotMonoDecompSettings();
+	settings.WriteNuGetPackageReferences = !result.Value.NoWriteNuGetPackageReferences;
+	settings.CopyOutOfTreeReferences = !result.Value.NoCopyOutOfTreeReferences;
+	settings.CreateAdditionalProjectsForProjectReferences = !result.Value.NoCreateAdditionalProjectsForProjectReferences;
+	settings.GodotVersionOverride = result.Value.GodotVersion == null ? null : GodotStuff.ParseGodotVersionFromString(result.Value.GodotVersion);
 
     // call the DecompileProject function
-    int result = Lib.DecompileProject(assemblyPath, outputPath, projectPath, referencePaths);
+    int resultCode = Lib.DecompileProject(assemblyPath, outputCSProj, projectPath, referencePaths, settings);
 
-    return result;
+    return resultCode;
 }
 
 return Main(args);
+
+public class Options
+{
+	[Value(0,MetaName = "AssemblyPath",HelpText = "path to the assembly to decompile")]
+	public string? AssemblyPath { get; set; }
+
+	[Option("force-version", Required = false, HelpText = "Set the Godot version to use for the decompilation.")]
+	public string? GodotVersion { get; set; }
+
+	[Option("output-dir", Required = false, HelpText = "Directory to output the project files to. If not specified, the project file will be output to the same directory as the assembly.")]
+	public string? OutputProject { get; set; }
+
+	[Option("project-name", Required = false, HelpText = "Name of the csproj file to output. If not specified, the name of the assembly will be used.")]
+	public string? ProjectName { get; set; }
+
+	[Option("reference-path", Required = false, HelpText = "Path to search for references. If not specified, the references will be searched for in the same directory as the assembly.")]
+	public string ReferencePath { get; set; }
+
+	[Option("extracted-project", Required = false, HelpText = "Path to the extracted Godot project directory, used for determining original file locations. If not provided, will be assumed to be the same directory as output-project.")]
+	public string? ExtractedProject { get; set; }
+
+	// the rest of the options in the GodotMonoDecompSettings class
+
+	[Option("no-write-package-references", Required = false, HelpText = "Whether to write NuGet package references to the project file if dependency information is available.")]
+	public bool NoWriteNuGetPackageReferences { get; set; }
+
+	[Option("no-copy-out-of-tree-references", Required = false, HelpText = "Whether to copy out-of-tree references (i.e. references that are not within the same directory structure as the project file) to the project file.")]
+	public bool NoCopyOutOfTreeReferences { get; set; }
+
+	[Option("no-multi-project", Required = false, HelpText = "Whether to create additional projects for project references in main module.")]
+	public bool NoCreateAdditionalProjectsForProjectReferences { get; set; }
+
+
+	[Option("verbose", Required = false, HelpText = "Set output to verbose messages.")]
+	public bool Verbose { get; set; }
+
+	[Option("help", Required = false, HelpText = "Show this help message and exit.")]
+	public bool Help { get; set; }
+
+
+}
