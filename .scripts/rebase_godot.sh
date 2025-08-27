a177defc596ff91d90fc3f2378e4192811d0d217
nikitalita <69168929+nikitalita@users.noreply.github.com>
2025-08-27 13:33:27 -0700

    add script to rebase our working branch onto current master with our patches

---
+++ .scripts/rebase_godot.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+SCRIPT_PATH=$(dirname "$0")
+cd "$SCRIPT_PATH/../../.."
+
+git fetch --all
+git checkout master
+git pull
+
+HEAD=$(git rev-parse HEAD)
+
+# check for the existence of the 'nikitalita' remote
+if ! git remote | grep -q "nikitalita"; then
+    git remote add nikitalita https://github.com/nikitalita/godot.git
+	git fetch nikitalita
+fi
+
+git checkout working-branch
+git reset --hard $HEAD
+
+BRANCHES_TO_MERGE=(
+	gltf-use-orm-texture
+	#convert-3.x-escn
+)
+
+for branch in "${BRANCHES_TO_MERGE[@]}"; do
+    # merge branch, use a merge commit
+    git merge nikitalita/$branch -m "Merge branch '$branch'"
+done
