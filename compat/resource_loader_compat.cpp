8bc9a1ca717798e909e7741bc4f204b4551ecd82
nikitalita <69168929+nikitalita@users.noreply.github.com>
2025-08-27 13:09:40 -0700

    ResourceCompatLoader::save_custom: ensure dir before saving

--- compat/resource_loader_compat.cpp
+++ compat/resource_loader_compat.cpp
@@ -572,6 +572,8 @@ bool ResourceCompatLoader::is_globally_available() {
 
 Error ResourceCompatLoader::save_custom(const Ref<Resource> &p_resource, const String &p_path, int ver_major, int ver_minor) {
 	ERR_FAIL_COND_V_MSG(ver_major <= 0, ERR_INVALID_PARAMETER, "Invalid version info");
+	Error err = gdre::ensure_dir(p_path.get_base_dir());
+	ERR_FAIL_COND_V_MSG(err != OK, err, "Could not ensure directory for " + p_path);
 	if (p_path.get_extension() == "tres" || p_path.get_extension() == "tscn") {
 		int ver_format = ResourceFormatSaverCompatText::get_default_format_version(ver_major, ver_minor);
 		ResourceFormatSaverCompatText saver;
