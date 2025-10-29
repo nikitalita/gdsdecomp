38bd0231c56175d86a84e1220d45ede0581cbd51
nikitalita <69168929+nikitalita@users.noreply.github.com>
2025-10-29 13:14:51 -0700

    Fix error reporting for resource_to_string

--- compat/resource_loader_compat.cpp
+++ compat/resource_loader_compat.cpp
@@ -451,25 +451,40 @@ String ResourceCompatLoader::resource_to_string(const String &p_path, bool p_ski
 	if (!FileAccess::exists(path)) {
 		orig_path = path;
 		path = GDRESettings::get_singleton()->get_mapped_path(path);
-		ERR_FAIL_COND_V_MSG(!FileAccess::exists(path), "", "File does not exist: " + path);
+		if (!FileAccess::exists(path)) {
+			return "ERROR: File does not exist: " + path;
+		}
 	}
 	if (path.get_file().to_lower() == "engine.cfb" || path.get_file().to_lower() == "project.binary") {
 		return ProjectConfigLoader::get_project_settings_as_string(path);
 	}
 	String ext = path.get_extension().to_lower();
 	if (ext == "tres" || ext == "tscn" || !handles_resource(path, "")) {
-		auto buf = FileAccess::get_file_as_bytes(path);
+		Error err;
+		Ref<FileAccess> f = FileAccess::open(path, FileAccess::READ, &err);
+		if (f.is_null() || err != OK) {
+			if (err == ERR_UNAUTHORIZED) {
+				return "ERROR: Can't read encrypted file: " + path;
+			}
+			return "ERROR: Failed to open file: " + path;
+		}
+		auto buf = f->get_buffer(f->get_length());
 		if (ext == "tres" || ext == "tscn" || gdre::detect_utf8(buf)) {
 			String str;
 			str.append_utf8((const char *)buf.ptr(), buf.size());
 			return str;
 		}
-		ERR_FAIL_V_MSG("", "Failed to detect UTF-8 encoding for " + path);
+		return "ERROR: Failed to detect UTF-8 encoding for " + path;
 	}
 
 	Error err = OK;
 	Ref<Resource> res = _load_for_text_conversion(path, orig_path, &err);
-	ERR_FAIL_COND_V_MSG(err != OK || res.is_null(), "", "Failed to load " + path);
+	if (err != OK || res.is_null()) {
+		if (err == ERR_UNAUTHORIZED) {
+			return "ERROR: Can't read encrypted file: " + path;
+		}
+		return "ERROR: Failed to load " + path;
+	}
 
 	int64_t length = FileAccess::get_size(path);
 	Ref<Script> script = res;
@@ -491,7 +506,9 @@ String ResourceCompatLoader::resource_to_string(const String &p_path, bool p_ski
 		save_path = path.get_basename() + (res->get_save_class() == "PackedScene" ? ".tscn" : ".tres");
 		err = saver.save_to_file(f, save_path, res, 0);
 	}
-	ERR_FAIL_COND_V_MSG(err != OK, "", "Failed to save resource '" + path + "'.");
+	if (err != OK) {
+		return "ERROR: Failed to save resource '" + path + "'.";
+	}
 	return f->whole_file_as_utf8_string();
 }
 
